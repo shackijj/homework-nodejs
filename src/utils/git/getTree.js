be70924bd25c0d997cd77c8992570b12d8f115fb
Kirill Shatskiy <kshatskiy@Kirills-Mini.iponweb.lan>
2018-03-20 16:09:00 +0300

    Add ls-tree processing and link-back

--- src/utils/git/getTree.js
+++ src/utils/git/getTree.js
@@ -13,14 +13,29 @@ function getTree (repoPath, commitHash, path = '') {
     git ls-tree ${commitHash}:${path};
   `;
   return execShell(cmd)
-    .then(stdout => stdout
-      .trim()
-      .split('\n')
-      .map(line => {
-        const [mode, type, hash, name] = line.split(/\s+/);
-        return { mode, type, hash, name };
-      })
-    );
+    .then(stdout => {
+      const result = stdout
+        .trim()
+        .split('\n')
+        .map(line => {
+          const [mode, type, hash, name] = line.split(/\s+/);
+          return { type, name };
+        });
+
+      if (path) {
+        const name = path
+          .split('/')
+          .filter(item => item !== '')
+          .slice(0, -1)
+          .join('/');
+
+        result.unshift({
+          type: 'link-back',
+          name
+        });
+      }
+      return result;
+    });
 };
 
 module.exports = getTree;
