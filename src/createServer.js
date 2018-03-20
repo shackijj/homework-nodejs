e65817b0498f08830543c5722e5646df84ca59f2
Kirill Shatskiy <kshatskiy@Kirills-Mini.iponweb.lan>
2018-03-20 12:28:22 +0300

    Add /log path in server's configuration

--- src/createServer.js
+++ src/createServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const getBranches = require('./utils/git/getBranches');
+const getBranchCommits = require('./utils/git/getBranchCommits');
 
 function createServer (config) {
   const app = express();
@@ -12,6 +13,12 @@ function createServer (config) {
   app.get('/', (req, res) => {
     getBranches(config.repoPath)
       .then(branches => res.render('branches', { branches }))
+      .catch(error => res.render('error', { error }));
+  });
+
+  app.get('/log', (req, res) => {
+    getBranchCommits(config.repoPath, req.query.ref)
+      .then(commits => res.render('log', { commits }))
       .catch(error => res.render('error', { error }));
   });
 
