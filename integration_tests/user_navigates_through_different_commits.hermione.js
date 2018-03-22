fce543b350f09e7ce48b50151e0bf69a6fb0baf4
Kirill Shatskiy <shaackiy@gmail.com>
2018-03-22 16:54:07 +0300

    Move repeated blocks to a separate functions. Add tests for second commit.

--- integration_tests/user_navigates_through_different_commits.hermione.js
+++ integration_tests/user_navigates_through_different_commits.hermione.js
@@ -1,3 +1,27 @@
+function checkFileJsContent (content) {
+  describe('User clicks on file.js', function () {
+    beforeEach(function () {
+      return this.browser.click('(//a[@class="blob-object__link"])[1]');
+    });
+    it('should show content of file.js', function () {
+      return this.browser
+        .assertTextOfElementEqual('(//pre[@class="blob__content"])[1]', content);
+    });
+  });
+}
+
+function checkFilesFromTheFirstCommit () {
+  it('should show file.js', function () {
+    return this.browser
+      .assertTextOfElementEqual('(//a[@class="blob-object__link"])[1]', 'file.js');
+  });
+
+  it('should show dir1', function () {
+    return this.browser
+      .assertTextOfElementEqual('(//a[@class="tree-object__link"])[1]', 'dir1');
+  });
+}
+
 describe('User opens the list of commits for branch test2.', function () {
   beforeEach(function () {
     return this.browser
@@ -9,24 +33,22 @@ describe('User opens the list of commits for branch test2.', function () {
       return this.browser.click('(//a[@class="commit__tree"])[2]');
     });
 
-    it('should show file.js', function () {
-      return this.browser
-        .assertTextOfElementEqual('(//a[@class="blob-object__link"])[1]', 'file.js');
+    checkFilesFromTheFirstCommit();
+    checkFileJsContent('File1 Branch: test1');
+  });
+
+  describe('User clicks on tree of the second commit of test2 branch.', function () {
+    beforeEach(function () {
+      return this.browser.click('(//a[@class="commit__tree"])[1]');
     });
 
-    it('should show dir', function () {
+    checkFilesFromTheFirstCommit();
+
+    it('should show dir2', function () {
       return this.browser
-        .assertTextOfElementEqual('(//a[@class="tree-object__link"])[1]', 'dir1');
+        .assertTextOfElementEqual('(//a[@class="tree-object__link"])[2]', 'dir2');
     });
 
-    describe('User clicks on file.js', function () {
-      beforeEach(function () {
-        return this.browser.click('(//a[@class="blob-object__link"])[1]');
-      });
-      it('should show content of file.js', function () {
-        return this.browser
-          .assertTextOfElementEqual('(//pre[@class="blob__content"])[1]', 'File1 Branch: test1');
-      });
-    });
+    checkFileJsContent('File1 Branch: test2');
   });
 });
